13418e95a05b5b25c5ba2e6739ccdb04ae946ba8
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-08-21 04:28:45 +0000

    Delete todo item

--- todo/views.py
+++ todo/views.py
@@ -45,4 +45,10 @@ def toggle_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     item.done = not item.done
     item.save()
-    return render(request, 'todo/edit_item.html')
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
