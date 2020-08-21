f262eda03fba09d2bf8ac0840add7160ab8fa9da
tracyjohnson213 <tracyjohnson213@gmail.com>
2020-08-21 01:20:25 +0000

    View Todo items

---
+++ todo/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .models import Item
+
+
+# Create your views here.
+def get_todo_list(request):
+    items = Item.objects.all()
+    context = {
+        'items': items
+    }
+    return render(request,
+                  'todo/todo_list.html',
+                  context)
